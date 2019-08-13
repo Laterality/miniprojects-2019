2df4bc888e861db0a3ca69063915082e30afa101
Jinwoo Shin <bluesky0255@gmail.com>
2019-08-13 14:41:07 +0900

    Add scripts
    - Added initializing script.
    - Added docker  image pushing script.
    - Added nginx configuration for nginx container.

---
+++ scripts/init.sh
@@ -0,0 +1,21 @@
+# Download docer-compose.yml, nginx configuration and static resources
+if [ $1 == "production" ]; then
+    git clone https://github.com/everyone-driven-development/miniprojects-2019 -b master --single-branch
+    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/master/docker-compose.yml > docker-compose.yml
+    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/master/scripts/nginx.conf > nginx.conf
+elif [ $1 == "develop" ]; then
+    git clone https://github.com/everyone-driven-development/miniprojects-2019 -b develop --single-branch
+    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/devlop/docker-compose.yml > docker-compose.yml
+    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/develop/scripts/nginx.conf > nginx.conf
+else
+    echo 'Argument [production, develop] is required'
+fi
+
+sudo snap install docker
+
+mkdir app
+cd app
+
+rm -rf backend
+
+sudo docker-compose up -d
