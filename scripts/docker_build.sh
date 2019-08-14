4157a0dd37ab4a533285936e91e6736d26c78907
Jinwoo Shin <bluesky0255@gmail.com>
2019-08-14 09:01:36 +0900

    add frontend dockerfile

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,2 +1,6 @@
+docker build -t wootube-view .
+
 cd backend
-docker build -t wootube --build-arg JAR_NAME=$JAR_NAME --build-arg JAR_PATH=./build/libs/$JAR_NAME .
+docker build -t wootube \
+    --build-arg JAR_NAME=$JAR_NAME \
+    --build-arg JAR_PATH=./build/libs/$JAR_NAME .
