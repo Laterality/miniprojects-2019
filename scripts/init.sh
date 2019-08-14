4d7e8c733df0d8e08a8a969165d95d892241f8a6
Jinwoo Shin <bluesky0255@gmail.com>
2019-08-14 11:22:27 +0900

    chore: change initialize script's download url temporarily

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,9 +4,13 @@ mkdir app
 cd app
 
 if [ $1 == "production" ]; then
-    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/master/docker-compose-production.yml > docker-compose.yml
+    # wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/master/docker-compose-production.yml > docker-compose.yml
+
+    wget -O - https://raw.githubusercontent.com/laterality/miniprojects-2019/develop/docker-compose-production.yml > docker-compose.yml
 elif [ $1 == "develop" ]; then
-    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/devlop/docker-compose-dev.yml > docker-compose.yml
+    # wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/devlop/docker-compose-dev.yml > docker-compose.yml
+
+    wget -O - https://raw.githubusercontent.com/laterality/miniprojects-2019/devlop/docker-compose-dev.yml > docker-compose.yml
 else
     echo 'Argument [production, develop] is required'
 fi
