533b7d6e7567115241b90e92d90a601165ffda53
Jinwoo Shin <bluesky0255@gmail.com>
2019-08-14 10:48:37 +0900

    update init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,18 +1,16 @@
 # Download docer-compose.yml, nginx configuration and static resources
+
+mkdir app
+cd app
+
 if [ $1 == "production" ]; then
-    git clone https://github.com/everyone-driven-development/miniprojects-2019 -b master --single-branch
-    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/master/docker-compose.yml > docker-compose.yml
-    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/master/scripts/nginx.conf > nginx.conf
+    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/master/docker-compose-production.yml > docker-compose.yml
 elif [ $1 == "develop" ]; then
-    git clone https://github.com/everyone-driven-development/miniprojects-2019 -b develop --single-branch
-    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/devlop/docker-compose.yml > docker-compose.yml
-    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/develop/scripts/nginx.conf > nginx.conf
+    wget -O - https://raw.githubusercontent.com/everyone-driven-development/miniprojects-2019/devlop/docker-compose-dev.yml > docker-compose.yml
 else
     echo 'Argument [production, develop] is required'
 fi
 
 sudo snap install docker
-
-rm -rf backend
 
 sudo docker-compose up -d
