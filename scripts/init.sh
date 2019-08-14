8075d1b0d65ac2a95c0de76f66da9a6000f37501
Jinwoo Shin <bluesky0255@gmail.com>
2019-08-14 17:37:10 +0900

    chore: remove unnecessary code

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,7 +4,6 @@ sudo snap install docker
 
 if [ $1 == "production" ]; then
     git clone -b EDD https://github.com/woowacourse/miniprojects-2019
-    sudo docker-compose -f docker-compose-productio
 elif [ $1 == "dev" ]; then
     git clone -b $3 https://github.com/$2/miniprojects-2019
 else
