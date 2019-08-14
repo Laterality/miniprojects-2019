3b10b2033446c6b1710dda49a8d0f0141faadcbe
Jinwoo Shin <bluesky0255@gmail.com>
2019-08-14 15:21:53 +0900

    chore: update init script to clone with specific branch

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,7 +6,7 @@ if [ $1 == "production" ]; then
     git clone -b EDD https://github.com/woowacourse/miniprojects-2019
     sudo docker-compose -f docker-compose-productio
 elif [ $1 == "dev" ]; then
-    git clone -b develop https://github.com/$2/miniprojects-2019
+    git clone -b $3 https://github.com/$2/miniprojects-2019
 else
     echo 'Argument [production, develop] is required'
 fi
